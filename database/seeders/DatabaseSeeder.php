eccbd9658ccad66af3519dd6b60392a4612baa66
Rituraj <riturajd265@gmail.com>
2021-09-17 23:56:29 +0530

    factories and seeders for post and users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->has(Post::factory()->count(3))->create();
     }
 }
