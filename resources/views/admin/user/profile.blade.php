25f73be4b07b9d17c6c5d5c9c05135fc106274e9
vagrant <vagrant@homestead>
2021-09-23 17:35:56 +0000

    basic blade structure of profile page

--- resources/views/admin/user/profile.blade.php
+++ resources/views/admin/user/profile.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 
-    This is the profile page
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            Profile details
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-body">
+                            Profile Edit seciton
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
